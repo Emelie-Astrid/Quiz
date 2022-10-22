c3c685efdcd01cb975b4e95e74cd07a65cae9731
Emelie-Astrid <emelie.almlof@gmail.com>
2022-10-23 00:05:15 +0200

    fixed results list, cleaned up code

--- index.js
+++ index.js
@@ -1,298 +1,223 @@
-//DIVAR - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-let test = document.querySelector("#quiz");
+//DIVS AND TEXT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+let quiz = document.querySelector("#quiz");
 let radio = document.querySelector("#radio");
-let submit = document.querySelector("#submit");
-
-//TEXT - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 let questionsAndPoints = document.querySelector("#question-number");
 let questionText = document.querySelector("#questions");
 let resultsText = document.querySelector("#quiz-result");
-let wrongHead = document.querySelector("#wrong-head");
-let wrongUl = document.querySelector("#wrong-answers");
+let answersUl = document.querySelector("#answers");
 
-//KNAPPAR - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//BUTTONS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 let darkBtn = document.querySelector("#dark-mode");
 let lightBtn = document.querySelector("#light-mode");
+let startBtn = document.querySelector("#start-quiz");
+let nextBtn = document.querySelector("#next-question");
+let resultBtn = document.querySelector("#show-results");
+let restartBtn = document.querySelector("#restart-quiz");
 
-//FRÅGOR OCH POÄNG - - - - - - - - - - - - - - - - - - - - - - - - -
+//QUESTION NUMBER AND POINTS - - - - - - - - - - - - - - - - - - - - - - - - -
 let questionNum = 0;
 let points = 0;
 
-//ARRAY MED QUIZ - - - - - - - - - - - - - - - - - - - - - - - - - -
+//ARRAY WITH QUIZ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 let myQuestions = [
-    // {
-    //     question: "Heter min katt Salsa?",
-    //     a: "Ja",
-    //     b: "Nej",
-    //     answer: "A",
-    //     type: "trueFalse",
-    // },
-    // {
-    //     question: "Heter jag Astrid i mellannamn?",
-    //     a: "Ja",
-    //     b: "Nej",
-    //     answer: "A",
-    //     type: "trueFalse",
-    // },
-    // {
-    //     question: "Jorden är platt",
-    //     a: "Ja",
-    //     b: "Nej",
-    //     answer: "B",
-    //     type: "trueFalse"
-    // },
-    {
-        question: "Himlen är blå",
-        a: "Ja",
-        b: "Nej",
-        answer: "A",
-        type: "trueFalse"
-    },
     {
         question: "Jag är sjuksköterska",
         a: "Ja",
         b: "Nej",
-        answer: "B",
-        type: "trueFalse"
-    },
-    {
-        question: "Kaffe",
-        a: "Ja",
-        b: "Nej",
-        answer: "A",
+        correctAnswers: "Nej",
         type: "trueFalse"
     },
     {
         question: "Hur gammal blev Astrid Lindgren?",
-        a: 89,
-        b: 91,
-        c: 94,
-        d: 97,
-        answer: "C",
+        a: "87",
+        b: "89",
+        c: "94",
+        d: "96",
+        correctAnswers: "94",
         type: "multipleChoice"
     },
-    // {
-    //     question: "Vilka av dessa karaktärer en katt? OBS, 1-3 rätt svar",
-    //     a: "Pelle",
-    //     b: "Cheshire",
-    //     c: "Pluto",
-    //     d: "Lufsen",
-    //     correctAnswers:["Pelle","Cheshire"], 
-    //     type: "checkBox"
-    // },
-    // {
-    //     question: "Vilken film är INTE från Studio Ghibli? OBS, 1-3 rätt svar",
-    //     a: "Askungen",
-    //     b: "YourName",
-    //     c: "The Wind Rises",
-    //     d: "Sprited Away",
-    //     correctAnswers:["Askungen","YourName"], 
-    //     type: "checkBox"
-    // },
+    {
+        question: "Vilka av dessa karaktärer en katt?",
+        a: "Pelle",
+        b: "Cheshire",
+        c: "Pluto",
+        d: "Lufsen",
+        correctAnswers:["Pelle", "Cheshire"], 
+        type: "checkBox"
+    },
+    {
+        question: "Vilken film är INTE från Studio Ghibli?",
+        a: "Askungen",
+        b: "YourName",
+        c: "The Wind Rises",
+        d: "Sprited Away",
+        correctAnswers:["Askungen","YourName"], 
+        type: "checkBox"
+    },
 ];
 
-// ARRAY MED FELAKTIGA SVAR - - - - - - - - - - - - - - - - - -
-let wrongArray = [];
+// ARRAY WITH ANSWERS - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+let answerArray = [];
 
-//FUNKTION DARK MODE LIGHT MODE - - - - - - - - - - - - - - - - - - -
+//FUNCTION DARK MODE LIGHT MODE - - - - - - - - - - - - - - - - - - - - - - -
+// FORTSÄTT HÄR! 
 
-
-//FUNKTION VISA FRÅGOR - - - - - - - - - - - - - - - - - - - - - - - - - -
-function showQuestions (){    
-
-    //Kollar antalet frågor mot array och skriver resultat
+//FUNKTION SHOW QUIZ - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+function showQuestions (){   
+    //Hide start button, show next button
+    startBtn.style.display = 'none';
+    nextBtn.removeAttribute("hidden");
+    
+    //Show points with color and hidden buttons
     if (questionNum >= myQuestions.length) {
 
-        //Färgar resultatet  - - - - - - - - - - - - - - - - - - - - - - 
+        resultBtn.removeAttribute("hidden");
+        restartBtn.removeAttribute("hidden");
+        
+        quiz.innerHTML = "<h2>Du fick " + points + " av " + myQuestions.length + " rätt!</h2>";
+        
         if ((points/questionNum) > 0.75) {
-            test.innerHTML = "<h2>Du fick " + points + " av " + myQuestions.length + " rätt!</h2>";
-            test.style.color = "green";
+            quiz.style.color = "green";
         }
         else if ((points/questionNum)<=0.74 && (points/questionNum)>=0.5){
-            test.innerHTML = "<h2>Du fick " + points + " av " + myQuestions.length + " rätt!</h2>";
-            test.style.color = "orange";
+            quiz.style.color = "orange";
         }
         else {
-            test.innerHTML = "<h2>Du fick " + points + " av " + myQuestions.length + " rätt!</h2>";
-            test.style.color = "red";
+            quiz.style.color = "red";
         }
-
-        resultsText.innerHTML = "Quiz klart!";
-        wrongHead.innerHTML = "Tyvärr svarade du fel på dessa frågor:"
-
-        // FORSÄTT JOBBA HÄR - - - - - - - - - - - - - - - - - - - - - - - -
-        // Varför blir alla svar gröna? Kolla igenom villkoren
-        
-
-        //Felaktiga frågor till en Ul
-        wrongArray.forEach((obj) => {
-            let wrongQuestion = document.createElement("li");
-            
-            if(obj.question.point === true){
-                wrongQuestion.style.color = "green";
-            }
-            else {
-                wrongQuestion.style.color = "red";
-            }
-            wrongQuestion.innerHTML = obj.question;
-            wrongUl.append(wrongQuestion);
-        })
-
-        questionNum = 0;
-        points = 0;
-
-        //Stoppar functionen när quiz är klar
+        //Stops quiz
         return false;
     }
     
-    //Skriver ut fråga xx av xx i en h2
+    //Current question out of total length of quiz
     questionsAndPoints.innerText = "Fråga " + (questionNum+1) + " av " + myQuestions.length;
-    currentQuestion = myQuestions[questionNum].question;
 
-    //MULTIPLE CHOICE frågor - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    //Text of questions
+    questionText.innerText = myQuestions[questionNum].question;
+
+    //Declare answers
+    let ansA = myQuestions[questionNum].a;
+    let ansB = myQuestions[questionNum].b;
+    let ansC = myQuestions[questionNum].c;
+    let ansD = myQuestions[questionNum].d;
+
+    //Empties div with radio buttons and checkboxes
+    radio.innerHTML= "";
+
+    //Creates checkboxes or radio buttons - - - - - - - - - - - - - - - - -
     if (myQuestions[questionNum].type === "multipleChoice") {
-    ansA = myQuestions[questionNum].a;
-    ansB = myQuestions[questionNum].b;
-    ansC = myQuestions[questionNum].c;
-    ansD = myQuestions[questionNum].d;
-    
-    //Skriver ut frågan
-    questionText.innerText = currentQuestion;
-
-    //Radio knappar
-    radio.innerHTML= "";
-    radio.innerHTML += '<label> <input type="radio" name="svarsAlt" value="A"> ' + ansA + 
-    '</label><br> <label> <input type="radio" name="svarsAlt" value="B"> ' + ansB + 
-    '</label><br> <label> <input type="radio" name="svarsAlt" value="C"> ' + ansC + 
-    '</label><br> <label> <input type="radio" name="svarsAlt" value="D"> ' + ansD + 
-    '</label><br><br>'
+        radio.innerHTML += `<label> <input type="radio" name="radioAns" value=${myQuestions[questionNum].a}> ${ansA} 
+        </label><br> <label> <input type="radio" name="radioAns" value=${myQuestions[questionNum].b}> ${ansB} 
+        </label><br> <label> <input type="radio" name="radioAns" value=${myQuestions[questionNum].c}> ${ansC} 
+        </label><br> <label> <input type="radio" name="radioAns" value=${myQuestions[questionNum].d}> ${ansD} 
+        </label><br><br>`
     }
     
-    //CHECKBOX frågor - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    else if (myQuestions[questionNum].type === "checkBox"){
-        ansA = myQuestions[questionNum].a;
-        ansB = myQuestions[questionNum].b;
-        ansC = myQuestions[questionNum].c;
-        ansD = myQuestions[questionNum].d;
-    
-    //Skriver ut frågan
-    questionText.innerText = currentQuestion;
-
-    //Checkbox knappar
-    radio.innerHTML= "";
-    radio.innerHTML += `<label> <input type="checkbox" name="svarsAltCheck" value=${myQuestions[questionNum].a}> ${ansA} 
-    </label><br> <label> <input type="checkbox" name="svarsAltCheck" value=${myQuestions[questionNum].b}> ${ansB} 
-    </label><br> <label> <input type="checkbox" name="svarsAltCheck" value=${myQuestions[questionNum].c}> ${ansC}
-    </label><br> <label> <input type="checkbox" name="svarsAltCheck" value=${myQuestions[questionNum].d}> ${ansD}
-    </label><br><br>`
+    else if (myQuestions[questionNum].type === "checkBox") {
+        radio.innerHTML += `<label> <input type="checkbox" value=${myQuestions[questionNum].a}> ${ansA} 
+        </label><br> <label> <input type="checkbox" value=${myQuestions[questionNum].b}> ${ansB} 
+        </label><br> <label> <input type="checkbox" value=${myQuestions[questionNum].c}> ${ansC}
+        </label><br> <label> <input type="checkbox" value=${myQuestions[questionNum].d}> ${ansD}
+        </label><br><br>`
     }
 
-    //TRUEFALSE frågor - - - - - - - - - - - - - - - - - - - - - - - - - - -
     else {
-    ansA = myQuestions[questionNum].a;
-    ansB = myQuestions[questionNum].b;
-    
-    //Skriver ut frågan
-    questionText.innerText = currentQuestion;
-    radio.innerHTML= "";
-    radio.innerHTML += '<label> <input type="radio" name="svarsAlt" value="A"> ' + ansA + 
-    '</label><br> <label> <input type="radio" name="svarsAlt" value="B"> ' + ansB + '</label><br><br>'
+        radio.innerHTML += `<label> <input type="radio" name="radioAns" value=${myQuestions[questionNum].a}> ${ansA} 
+        </label><br> <label> <input type="radio" name="radioAns" value=${myQuestions[questionNum].b}> ${ansB}  
+        </label><br><br>`
     }
-
-    //Resultatknapp
-    submit.innerHTML = "";
-    submit.innerHTML += "<button id='submit-btn' onclick='checkAnswers()'>Submit Answer</button>";
-
 }
 
-//FUNKTION TAR EMOT SVAR - - - - - - - - - - - - - - - - - - - - - - - - - 
-
+//FUNCTION CHECK ANSWERS - - - - - - - - - - - - - - - - - - - - - - - - - -
 function checkAnswers () {
 
-    //Hämta svar från checkboxar
+    //Answers from checkboxes
     if (myQuestions[questionNum].type === "checkBox"){
 
         let allCheckedBoxes = document.querySelectorAll("input[type='checkbox']:checked");
         let checkBoxPoints = [];
 
         //Tar emot alla boxar som är checkade
-        allCheckedBoxes.forEach((obj) => {
-            checkBoxPoints.push(obj.value);
+        allCheckedBoxes.forEach((box) => {
+            checkBoxPoints.push(box.value);
         });
 
-        let filteredPoints = checkBoxPoints.filter((check) => {
+        let pointsArr = checkBoxPoints.filter((check) => {
             return (myQuestions[questionNum].correctAnswers.includes(check)) 
         });
 
-
-        if ((filteredPoints.length === myQuestions[questionNum].correctAnswers.length) && (checkBoxPoints.length ===myQuestions[questionNum].correctAnswers.length)) {
-            points++
+        if ((pointsArr.length === myQuestions[questionNum].correctAnswers.length) && 
+            (checkBoxPoints.length === myQuestions[questionNum].correctAnswers.length)) {
+            points++;
+            answerArray.push({
+                question:myQuestions[questionNum].question, 
+                correctAnswers:myQuestions[questionNum].correctAnswers, 
+                point: true})
         }
         else {
-            wrongArray.push(myQuestions[questionNum].question);
+            answerArray.push({
+                question: myQuestions[questionNum].question,
+                correctAnswers:myQuestions[questionNum].correctAnswers, 
+                point: false})
         }
         }
         
-    //Hämtar svar från radiobuttons
+    //Answers from radiobuttons
     else {
-        svarsAlt = document.getElementsByName("svarsAlt");
-        for (let i=0; i<svarsAlt.length; i++) {
-        if (svarsAlt[i].checked) {
-            svar = svarsAlt[i].value;
+        let radioArr = document.getElementsByName("radioAns");
+        for (let i=0; i<radioArr.length; i++) {
+            if (radioArr[i].checked) {
+            ans = radioArr[i].value;
         }
     }
     //kollar om checked answer matchar korrekt val
-    if(svar === myQuestions[questionNum].answer) {
+    if (ans === myQuestions[questionNum].correctAnswers) {
         points++;
-        wrongArray.push({question:myQuestions[questionNum].question, point: true})
+        answerArray.push({question:myQuestions[questionNum].question, 
+            correctAnswers:myQuestions[questionNum].correctAnswers, 
+            point: true})
     }
     else {
-        // wrongArray.push(myQuestions[questionNum].question);
-
-        wrongArray.push({
-            question: myQuestions[questionNum].question, 
+        answerArray.push({question:myQuestions[questionNum].question, 
+            correctAnswers:myQuestions[questionNum].correctAnswers, 
             point: false})
 
     }
     }
-
     questionNum++;
     showQuestions();
 }
 
-console.log(wrongArray);
+//FUNCTION SHOW RESULTS - - - - - - - - - - - - - - - - - - - - - - - - - -
+function showResults () {
+    answerArray.forEach((obj) => {
+        let answers = document.createElement("li");
+        if(obj.point === true){
+            answers.innerHTML =`${obj.question}<br><br>`;
+            answers.style.color = "green";
+        }
+        else {
+            answers.innerHTML =`${obj.question}<br>Rätt svar:  ${obj.correctAnswers}<br><br>`;
+            answers.style.color = "red";
+        }
+        answersUl.append(answers);
+    })
+    resultBtn.style.display = "none";
+}
 
+//BUTTONS - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+startBtn.addEventListener("click", () => {
+    showQuestions();
+});
 
-window.addEventListener("load", showQuestions);
+nextBtn.addEventListener("click", () => {
+    checkAnswers();
+})
 
+resultBtn.addEventListener("click", () => {
+    showResults();
+});
 
-
-
-
-
-
-
-
-//OLIKA BRA Console.log
-//Min array
-// console.log(myQuestions);
-//Vilken typ av fråga
-// console.log(myQuestions[questionNum].type);
-//Om typen av fråga är "xxx"
-// console.log(myQuestions[questionNum].type === "multipleChoice");
-
-
-//Kollar om event ändrats på check-box
-// allCheckedBoxes.forEach((box) => {
-//     box.checked = false;
-//     box.addEventListener("change", () => filteredPoints());
-// });
-
-// //Kollar igen om checkboxar är icheckade och skriver in värdet i arr
-// function grabCheckBoxValues() {
-//     allCheckedBoxes.forEach((checkBox) => {
-//         if(checkBox.checked)
-//         checkBoxPoints.push(checkBox.value);
-//     });
-//     return checkBoxPoints;
-// }
+restartBtn.addEventListener("click", () => {
+    window.location.reload();
+});
